26a8337fc8553956be90cf898274e04ebeaebf93
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-05 20:59:33 -0500

    namespace with /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :weddings do
-    resources :guests
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :weddings do
+        resources :guests
+      end
+    end
   end
 end
