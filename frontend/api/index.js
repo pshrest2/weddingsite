ffec45a52e0530b5734c07fb7fb6205831800ee6
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-05 19:55:02 -0500

    create CRUD Api call functions for /guests and /weddings

---
+++ frontend/api/index.js
@@ -0,0 +1,47 @@
+const API = {
+  securedFetch: async (route, body, opts = {}) => {
+    const options = { ...opts };
+
+    if (body) {
+      options.body = JSON.stringify(body);
+      options.method = options.method || "POST";
+      options.headers = {
+        ...options.headers,
+        "Content-Type": "application/json",
+      };
+    }
+
+    return await fetch(route, options);
+  },
+
+  fetchJSON: (route, body, opts = {}) =>
+    API.securedFetch(route, body, opts).then((response) => {
+      if (response.ok) {
+        if (response.headers.get("content-type").match(/application\/json/i)) {
+          return response.json();
+        }
+        return new Promise((resolve, reject) => reject(response));
+      }
+      if (response.status >= 400) {
+        return new Promise((resolve, reject) => {
+          response
+            .json()
+            .then((json) => {
+              reject(json);
+            })
+            .catch(() => {
+              reject({
+                error: {
+                  type: "FetchParseError",
+                  message:
+                    "A 422 error was thrown, but the response could not be parsed as JSON",
+                },
+              });
+            });
+        });
+      }
+      return new Promise((resolve, reject) => reject(response));
+    }),
+};
+
+export default API;
